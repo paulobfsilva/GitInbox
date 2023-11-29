22c429e88ae3f96d26cea9013a028945f7293c38
Paulo Silva <paulobfsilva@gmail.com>
2023-11-29 18:46:24 +0000

    e - New Xcode project with Xcode 15.0.1, SwiftUI app; Language: Swift; Unit Tests on

---
+++ GitInbox/GitInboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitInboxApp.swift
+//  GitInbox
+//
+//  Created by paulo on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitInboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
