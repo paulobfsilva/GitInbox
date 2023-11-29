3a1dfbae2ff7c7227db6970baa834f0918d66052
Paulo Silva <paulobfsilva@gmail.com>
2023-11-29 21:23:02 +0000

    R!! don't launch regular app during tests

--- GitInbox/Main.swift
+++ GitInbox/Main.swift
@@ -10,10 +10,10 @@ import SwiftUI
 @main
 struct Main {
     static func main() {
-//        if NSClassFromString("XCTestCase") != nil {  
-//            TestApp.main()
-//        } else {
+        if NSClassFromString("XCTestCase") != nil {
+            TestApp.main()
+        } else {
             GitInboxApp.main()
-//        }
+        }
     }
 }
---
+++ GitInbox/TestApp.swift
@@ -0,0 +1,14 @@
+//
+//  TestApp.swift
+//  GitInbox
+//
+//  Created by paulo on 29/11/2023.
+//
+
+import SwiftUI
+
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {}
+    }
+}
