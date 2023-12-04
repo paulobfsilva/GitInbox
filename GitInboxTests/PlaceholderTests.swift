ea0971219851604cf71068a4d14e8461a25433ad
Paulo Silva <paulobfsilva@gmail.com>
2023-12-04 20:53:24 +0000

    t - replace Apple placeholder tests with our fast one

---
+++ GitInboxTests/PlaceholderTests.swift
@@ -0,0 +1,15 @@
+//
+//  PlaceholderTests.swift
+//  GitInboxTests
+//
+//  Created by paulo on 04/12/2023.
+//
+
+@testable import GitInbox
+import XCTest
+
+@MainActor
+final class PlaceholderTests: XCTestCase {
+    func test_zero() throws {
+    }
+}
